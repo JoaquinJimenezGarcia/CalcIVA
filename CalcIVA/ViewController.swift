0272f45baccf615dbf4a20f682a5157c91aed557
Joaquin Jimenez <garcia.jjimenez@gmail.com>
2017-03-27 16:09:59 +0200

    Initial Commit

---
+++ CalcIVA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcIVA
+//
+//  Created by Joaquín Jiménez García on 27/3/17.
+//  Copyright © 2017 Joaquín Jiménez García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
